0d2ec1811c9b48314fdecc1a4a88ac7ce0bea467
Rusbent Matta <rusbent_matta@itrsa.com.ar>
2021-08-13 09:37:45 -0300

    recaptcha funtional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   title = 'pg-recaptcha';
-  protected aFormGroup: FormGroup | undefined;
+  public aFormGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(public formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.aFormGroup = this.formBuilder.group({
       recaptcha: ['', Validators.required]
     });
   }
+
+  siteKey:string="6LclStUbAAAAAI6Nlc9c1UiZ5MJeWBtggxCZaX7D";
 
   
 }
